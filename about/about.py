030d060687a10539e2c4ee15222a9f4cc55e3686
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2014-10-28 14:13:27 +0100

    version 4.0.0

--- about/about.py
+++ about/about.py
@@ -2,7 +2,7 @@
 
 metadata = dict(
   __name__        = "about",
-  __version__     = "4.0.0-beta",
+  __version__     = "4.0.0",
   __license__     = "MIT License",  
   __author__      = u"Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>",
   __url__         = "https://warehouse.python.org/project/about",
